468ac0b96398102a522fc728f6a8f69665a58994
BRAYANPG987 <BRAYANPG@JARANDES.COM>
2024-10-12 09:43:11 -0500

    Actualizacion

---
+++ Actividad4.py
@@ -0,0 +1,29 @@
+# Actividad 4: Hola mundo
+
+print("Hola mundo")
+
+# Actividad 4: Ejercicio 1: Crear Variables
+# Crea variables para almacenar los siguientes datos:
+
+# Tu edad (entero).
+# Tu altura en metros (flotante).
+# Tu nombre completo (cadena).
+# Si tienes mascota (booleano).
+# Una lista con los nombres de tres amigos.
+
+edad = 20
+altura = 1.73
+nombre = "Brayan"
+mascota = True
+amigos = ["Duver", "David", "Jojan"]
+
+print(f"Mi nombre es {nombre} y tengo {edad} años")
+
+# Actividad 5 Realiza un if donde determine si una persona es mayor de edad o no
+
+edad = 20 
+if edad > 18:
+    print("La persona es mayor de edad")
+else:
+    print("La persona es menor de edad")
+
